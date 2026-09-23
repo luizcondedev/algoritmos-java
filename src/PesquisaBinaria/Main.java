ce59f4ff8dced2b3814168153b38a24effbce706
Luiz Conde <luizconde.deev@gmail.com>
2026-09-23 13:41:37 -0300

    Cap 1: Revisão de Pesquisa Binaria

--- src/PesquisaBinaria/Main.java
+++ src/PesquisaBinaria/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] lista = {1, 3, 5, 7, 9, 11, 15, 20};
 
-        System.out.println(PesquisaBinaria.pesquisaBinaria(lista, 7));
+        System.out.println(PesquisaBinaria.pesquisaBinaria(lista, 20));
         System.out.println(PesquisaBinaria.pesquisaBinaria(lista, 8));
     }
 }
